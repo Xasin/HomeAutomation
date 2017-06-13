7dea66c21d06c0d99bdfce14f66f0b0cca80e4ea
David Bailey <davidbailey.2889@gmail.com>
2017-06-13 18:10:14 +0200

    Made a tiny Swich-Informing daemon script!

---
+++ Projects/SwitchCounter/SwitchNote.rb
@@ -0,0 +1,39 @@
+require 'mqtt'
+
+Process.setproctitle("switchNote");
+Process.daemon();
+require_relative 'Ruby/credentials'
+
+$connectAttempts = 0;
+def MQTT_reconnect()
+	return if $connecting;
+
+	$connecting = true;
+
+	while true
+		begin
+			$xaQTT = MQTT::Client.connect(host: '192.168.178.111', port: 1883, username: $mqtt_credentials[:username], password: $mqtt_credentials[:passwd], client_id: "SwitchNote-#{`hostname`}", clean_session: false);
+		rescue
+			$connectAttempts += 1;
+			print "Connecting (#{$connectAttempts})\n"
+			sleep 10;
+		else
+			$connecting = false;
+			$connectAttempts = 0;
+			return;
+		end
+	end
+end
+
+while true
+	begin
+		$xaQTT.get('personal/switching/+/who') do |topic, payload|
+			sysName = topic.match(/^personal\/switching\/(\w+)\/who$/)[1];
+			`notify-send 'SwitchTime' 'System #{sysName} has just switched over to #{payload}'`
+			sleep 5;
+		end
+
+	rescue
+		MQTT_reconnect();
+	end
+end
