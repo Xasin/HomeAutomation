1663667d48bc5e3b1395a54dde9886ef11ec7264
David Bailey <davidbailey.2889@gmail.com>
2017-08-24 15:14:12 +0200

    Added 'set' operators for color and brightness

--- Projects/HomeSweetHome/ColorSpeak.rb
+++ Projects/HomeSweetHome/ColorSpeak.rb
@@ -14,19 +14,37 @@ class ColorSpeak
 			h[k] = Array.new();
 		end
 
-		mqtt.subscribeTo "TTS/+" do |t, data|
+		mqtt.subscribeTo "Room/TTS/+" do |t, data|
 			h = JSON.parse(data);
 			return unless h.key? "text";
 
 			c = h.key?("color") ? Color.from_s(h["color"]) : nil;
 			queueWords(t[0], h["text"], c);
 		end
+
+		mqtt.subscribeTo "Room/Light/Set/Color" do |t, data|
+			@defaultC = Color.from_s(data);
+			updateDefaultColor();
+		end
+
+		mqtt.subscribeTo "Room/Light/Set/Brightness" do |t, data|
+			@defaultC.set_brightness(data.to_i);
+			updateDefaultColor();
+		end
+	end
+
+	def updateDefaultColor()
+		mqtt.publishTo "Room/Light/Color", @defaultC.to_s;
+		mqtt.publishTo "Room/Light/Brightness", @defaultC.get_brightness;
+
+		@led.sendRGB(@defaultC, 3) unless @speaking;
 	end
 
 	def queueWords(id, t, c)
 		@speechQueue[id].push({t: t, c: c});
 
 		return if @speaking;
+
 		@speaking = true;
 		@speechThread = Thread.new() {
 			speakOutQueue();
@@ -43,13 +61,14 @@ class ColorSpeak
 			while h = v.shift
 				next if h[:t] =~ /[^\w\s\.,-:+']/;
 
-				@led.sendRGB(h[:c], 0.5) unless h[:c] == nil;
+				@led.sendRGB(h[:c] ? h[:c] : @defaultC, 0.5);
 				system('espeak -s 150 -g 3 "' + h[:t] + '" --stdout 2>/dev/null | aplay >/dev/null 2>&1');
 			end
 
 			@speechQueue.delete k;
 		end
-		@led.sendRGB(@defaultC, 0.5);
+
+		@led.sendRGB(@defaultC, 3);
 		@speaking = false;
 	end
 end
