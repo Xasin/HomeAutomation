3f3920b3ed396493aaf598b46e20352585ee39a9
David Bailey <davidbailey.2889@gmail.com>
2017-09-25 21:57:50 +0200

    Added core extensions to default load.

--- Projects/HomeSweetHome/SetupEnv.rb
+++ Projects/HomeSweetHome/SetupEnv.rb
@@ -1,6 +1,7 @@
 
 require_relative 'RoomLight/ColorSpeak.rb'
 require_relative 'Libs/MQTTSubscriber.rb'
+require_relative 'Libs/CoreExtensions.rb'
 
 login = File.read(File.expand_path("~/.HoT/logins/snippet.login")).strip!
 
