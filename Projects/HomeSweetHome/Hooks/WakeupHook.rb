a3cc5fa5042085b46b84d876d75e98f65be12aea
David Bailey <davidbailey.2889@gmail.com>
2017-09-26 21:20:25 +0200

    Added an option for the daily weather forecast!

--- Projects/HomeSweetHome/Hooks/WakeupHook.rb
+++ Projects/HomeSweetHome/Hooks/WakeupHook.rb
@@ -1,25 +1,53 @@
 require_relative '../SetupEnv.rb'
+require_relative '../weather.rb'
 
 module Hooks
 	module Wakeup
 
-		@wakeupTTS = ColorSpeak::Client.new($mqtt, "Wakeup");
+		@wakeupTTS  = ColorSpeak::Client.new($mqtt, "Wakeup");
+		@weatherTTS = ColorSpeak::Client.new($mqtt, "Weather");
 
 		@alarmTime = nil;
 		@AlarmThread = Thread.new do
 			while true do
-				until @alarmTime do Thread.stop(); end
+				Thread.stop() until @alarmTime;
+
+				sleep [0.5, [2.minutes, (@alarmTime - Time.now())*0.9].min].max
 
 				if(Time.now() >= @alarmTime) then
+					@weatherTime = @alarmTime + 5.minutes;
+					@WeatherThread.run();
+
+					@alarmTime = nil;
+
 					$mqtt.publishTo "Room/Commands", "good morning"
 					$mqtt.publishTo "Room/Light/Set/Switch", "on";
 					$mqtt.publishTo "Room/Light/Set/Color", Color.RGB(0, 0, 0);
 
 					@wakeupTTS.speak "Good morning, David"
+				end
+			end
+		end
 
-					@alarmTime = nil;
-				else
-					sleep [0.5, [2.minutes, (@alarmTime - Time.now())*0.9].min].max
+		@weatherTime = nil;
+		@WeatherThread = Thread.new do
+			while true do
+				Thread.stop() until @weatherTime;
+
+				sleep [0.5, [2.minutes, (@weatherTime - Time.now())*0.9].min].max
+
+				if(Time.now() >= @weatherTime) then
+					@weatherTime = nil;
+
+					@weatherTTS.speak "And now, the weather report: "
+					sleep 5;
+
+					first = true;
+					$weather.fiveday_data["list"].each do |d|
+						break if d["dt"].to_i >= Time.today(21.hours).to_i;
+						@weatherTTS.speak $weather.readable_forecast(d, temperature: true, forceDay: first), Color.HSV(120 - 100*(d["main"]["temp"].to_i - 17)/5);
+						first = false;
+					end
 				end
 			end
 		end
