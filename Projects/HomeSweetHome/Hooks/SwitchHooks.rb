9e3a60b6af7ff96a6d9091ab5be843e5e01a619e
David Bailey <DavidBailey.2889@gmail.com>
2017-10-19 17:06:05 +0200

    Added protection against double switches.

--- Projects/HomeSweetHome/Hooks/SwitchHooks.rb
+++ Projects/HomeSweetHome/Hooks/SwitchHooks.rb
@@ -14,14 +14,18 @@ module Hooks
 		@formerMember = "none";
 
 		$mqtt.subscribe_to "personal/switching/Xasin/who" do |topic, data|
-			if(data != "none" and @formerMember == "none") then
-				@switchTTS.speak "Good morning, #{data}", @SystemColors[data], single: true;
-			elsif(data != "none") then
-				@switchTTS.speak "Hello, #{data}", @SystemColors[data], single: true;
-			elsif(@formerMember != "none") then
-				@switchTTS.speak "Good night, #{@formerMember}", @SystemColors[@formerMember], single: true;
+			$switchedInMember = data;
+
+			if(data != @formerMember) then
+				if(data != "none" and @formerMember == "none") then
+					@switchTTS.speak "Good morning, #{data}", @SystemColors[data], single: true;
+				elsif(data != "none") then
+					@switchTTS.speak "Hello, #{data}", @SystemColors[data], single: true;
+				elsif(@formerMember != "none") then
+					@switchTTS.speak "Good night, #{@formerMember}", @SystemColors[@formerMember], single: true;
+				end
+				@formerMember = data;
 			end
-			@formerMember = data;
 		end
 	end
 end
