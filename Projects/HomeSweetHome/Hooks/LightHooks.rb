e82dd078e3fda82309ca95c54c3e3f45febb2c0c
David Bailey <davidbailey.2889@gmail.com>
2017-10-30 14:59:13 +0100

    Moved the hooks for command-light control to separate file

---
+++ Projects/HomeSweetHome/Hooks/LightHooks.rb
@@ -0,0 +1,23 @@
+
+module Hooks
+	module Lights
+		@switchValue = $mqtt.track "Room/default/Lights/Switch"
+
+		@mqtt.subscribe_to "Room/#{@RoomName}/Commands" do |tList, data|
+			if(data == "e") then
+				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Switch", @switchValue.value == "on" ? "off" : "on", retain: true;
+			elsif(data == "ld") then
+				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Switch", "on", retain: true;
+				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Color", Color.RGB(0,0,0).to_s, retain: true;
+			elsif(data =~ /lh([\d]{1,3})/) then
+				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Switch", "on", retain: true;
+				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Color", Color.HSV($~[1].to_i).to_s, retain: true;
+			elsif(data =~ /l([\da-f]{6})/) then
+				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Switch", "on", retain: true;
+				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Color", Color.from_s("#" + $~[1]).to_s, retain: true;
+			elsif(data == "gn") then
+				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Switch", "off", retain: true;
+			end
+		end
+	end
+end
--- Projects/HomeSweetHome/RoomLight/ColorSpeak.rb
+++ Projects/HomeSweetHome/RoomLight/ColorSpeak.rb
@@ -44,23 +44,6 @@ class Server
 			update_current_color();
 		end
 
-		@mqtt.subscribe_to "Room/#{@RoomName}/Commands" do |tList, data|
-			if(data == "e") then
-				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Switch", @lightOn ? "off" : "on", retain: true;
-			elsif(data == "ld") then
-				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Switch", "on", retain: true;
-				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Color", Color.RGB(0,0,0).to_s, retain: true;
-			elsif(data =~ /lh([\d]{1,3})/) then
-				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Switch", "on", retain: true;
-				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Color", Color.HSV($~[1].to_i).to_s, retain: true;
-			elsif(data =~ /l([\da-f]{6})/) then
-				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Switch", "on", retain: true;
-				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Color", Color.from_s("#" + $~[1]).to_s, retain: true;
-			elsif(data == "gn") then
-				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Switch", "off", retain: true;
-			end
-		end
-
 		Thread.new() do
 			while true do
 				sleep 10
@@ -126,7 +109,7 @@ class Server
 				h[:text].gsub!(/[^\w\s\.,-:+']/, " ");
 
 				@speaking = true;
-					speechBrightness = [get_recommended_color().get_brightness, 50].max();
+					speechBrightness = [get_recommended_color().get_brightness, 20].max();
 					@led.sendRGB(h[:color] ? h[:color].set_brightness(speechBrightness) : get_current_color, 0.5);
 					system('espeak -s 140 -g 3 -a 200 "' + h[:text] + '" --stdout 2>/dev/null | aplay >/dev/null 2>&1');
 				@speaking = false;
