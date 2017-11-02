6cd97614f48ba3a1081b95e75281a919f4e9eedb
David Bailey <davidbailey.2889@gmail.com>
2017-11-02 14:14:54 +0100

    Added "mix-together" daylight calculation Lots of dynamic! Whee~

--- Projects/HomeSweetHome/Hooks/LightHooks.rb
+++ Projects/HomeSweetHome/Hooks/LightHooks.rb
@@ -1,4 +1,7 @@
 
+require 'interpolate'
+require_relative '../Libs/InterpolateTools.rb'
+
 module Hooks
 	module Lights
 		@switchValue = $mqtt.track "Room/default/Lights/Switch"
@@ -18,6 +21,56 @@ module Hooks
 			elsif(data == "gn") then
 				$mqtt.publish_to "Room/#{@RoomName}/Lights/Set/Switch", "off", retain: true;
 			end
+		end
+
+		@daylightProfile = {};
+		dayProfile = {
+			1.hours	=> Color.K(1000, 0.2),
+			6.hours 	=> Color.K(1000, 0.2),
+			8.hours 	=> Color.K(4000, 1),
+			10.hours	=> Color.K(5500, 1),
+			15.hours => Color.K(5000, 1),
+			18.hours => Color.K(3000, 1),
+			20.hours	=> Color.K(3000, 1),
+			24.hours => Color.K(1800, 0.5),
+		};
+
+		7.times do |i|
+			Interpolate::mix_looped(@daylightProfile, dayProfile, offset: i.days, upperBound: 7.days, spacing: 0.5.hours);
+		end
+
+		wakeupTimes = [7.hours, 7.hours, 7.hours, 7.5.hours, 7.5.hours, 9.5.hours, 9.5.hours];
+		wakeupProfile = {
+			-1.minutes => Color.K(1800, 0.1),
+			15.minutes => Color.K(4000, 1),
+		};
+		7.times do |i|
+			Interpolate::mix_looped(@daylightProfile, wakeupProfile, offset: i.days + wakeupTimes[i], upperBound: 7.days, spacing: 0.5.hours);
+		end
+
+		workoutTimes = [18.hours, 17.15.hours, 20.hours, 18.hours, 18.hours, 18.hours, 18.hours];
+		workoutProfile = {
+			0 				=> Color.K(6000, 1),
+			18.minutes	=> Color.K(6000, 1),
+		}
+		7.times do |i|
+			Interpolate::mix_looped(@daylightProfile, workoutProfile, offset: i.days + workoutTimes[i], upperBound: 7.days, spacing: 15.minutes);
+		end
+
+		teaTimes = [15.hours, 16.45.hours, 16.45.hours, 17.hours, 17.hours, 17.hours, 17.hours];
+		teaProfile = {
+			-3.minutes 	=> Color.K(2400, 0.7),
+			20.minutes 	=> Color.K(2400, 0.7),
+		}
+		7.times do |i|
+			Interpolate::mix_looped(@daylightProfile, teaProfile, offset: i.days + teaTimes[i], upperBound: 7.days, spacing: 10.minutes);
+		end
+
+		@daylightInterpolator = Interpolate::Points.new(@daylightProfile);
+		$cSpeak.daylight_getter do
+			t = Time.now();
+			currentTime = ((t.wday-1)%7).days + t.hour.hours + t.min.minutes + t.sec
+			@daylightInterpolator.at(currentTime);
 		end
 	end
 end
