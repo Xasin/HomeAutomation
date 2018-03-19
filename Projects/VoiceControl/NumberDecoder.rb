cd2b6dedddfd3b618e22df3d9678cecacf6848fc
Xasin <logger89@web.de>
2018-03-19 12:24:05 +0100

    Added first version of a number decoder

--- Projects/VoiceControl/NumberDecoder.rb
+++ Projects/VoiceControl/NumberDecoder.rb
@@ -31,5 +31,68 @@ module VoiceControl
 			"eighty"		=> 80,
 			"ninety"		=> 90,
 		}
+
+		def self.decode(stringArray)
+			stringArray = stringArray.clone;
+			stringArray = stringArray.split(" ") unless stringArray.is_a? Array
+
+			total = 0;
+			currentBlock = 0;
+
+			outputString = [];
+			stage = :idle;
+
+			stringArray.each do |e|
+				next if (e == "and") and stage != :idle;
+
+				case stage
+				when :idle
+					if @numbers.include? e then
+						stage = :number;
+						redo;
+					end
+					outputString << e;
+
+				when :number
+					if @numbers.include? e then
+						currentBlock += @numbers[e];
+					elsif e == "hundred" then
+						currentBlock *= 100;
+					elsif @modifiers.include? e then
+						stage = :modifier;
+						redo;
+					else
+						total += currentBlock;
+						currentBlock = 0;
+						outputString << total;
+						total = 0;
+						stage = :idle;
+						redo;
+					end
+
+				when :modifier
+					if @modifiers.include? e then
+						currentBlock *= @modifiers[e];
+					elsif @numbers.include? e then
+						total += currentBlock;
+						currentBlock = 0;
+						stage = :number;
+						redo;
+					else
+						total += currentBlock;
+						currentBlock = 0;
+						outputString << total;
+						total = 0;
+						stage = :idle;
+						redo;
+					end
+				end
+			end
+
+			total += currentBlock;
+			outputString << total if stage != :idle;
+
+			return outputString.join(" ");
+		end
 	end
 end
