2077df365426fdc413479b6492252a4fcbca3ce3
Xasin <logger89@web.de>
2018-04-06 15:24:24 +0200

    Updated WelcomeBack hook

--- Projects/HomeSweetHome/Hooks/WelcomeBack.rb
+++ Projects/HomeSweetHome/Hooks/WelcomeBack.rb
@@ -1,33 +1,25 @@
 module Hooks
 module Welcome
 
-@SystemColors = {
-	"Xasin" => Color.RGB(255, 50, 50),
-	"Neira" => Color.RGB(50, 50, 255),
-	"Mesh"  => Color.RGB(50, 255, 50),
-}
-
-
 @welcomeTTS = ColorSpeak::Client.new($mqtt, "Welcome");
 
-@switchTrack 	= $mqtt.track "Personal/Xasin/Switching/Who";
 @computerTrack = $mqtt.track "Room/default/X-Desktop/Status"
 
 $mqtt.track "Personal/Xasin/IsHome" do |data|
 	if(data == "true") then
-		if(@SystemColors.key? @switchTrack.value)
-			who = @switchTrack.value;
+		if($xasin.awake?)
+			who = $xasin.switch;
 
-			@welcomeTTS.speak "Welcome back home #{who}", @SystemColors[who];
+			@welcomeTTS.speak "Welcome back home #{who}", Hooks::SwitchHooks::SystemColors[who];
 			if(Time.today($lightsOnTime) < Time.now()) then
-				$mqtt.publish_to "Room/default/Lights/Set/Switch", "on"
+				$room.lights = true;
 			end
 
 			`etherwake 54:a0:50:50:d6:ac` if(@computerTrack.value == "SUSPENDED");
 		end
 	else
 		$telegram.send_message("See you around!", disable_notification: true);
-		$mqtt.publish_to "Room/default/Lights/Set/Switch", "off"
+		$room.lights = false;
 	end
 end
 
