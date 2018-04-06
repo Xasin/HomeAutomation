042ab78c26059d4d3361495050dd0c9a3edd2ecc
Xasin <logger89@web.de>
2018-04-06 14:49:28 +0200

    Added light getter and on_command block

--- Projects/HomeSweetHome/Convenience/Room.rb
+++ Projects/HomeSweetHome/Convenience/Room.rb
@@ -4,6 +4,14 @@ module Convenience
 		def initialize(mqtt, name)
 			@mqtt = mqtt;
 			@name = name;
+
+			@lights 			= @mqtt.track "Room/#{@name}/Lights";
+			@lightSwitch	= @mqtt.track "Room/#{@name}/Switch";
+
+			@commandBlocks = Array.new();
+			@mqtt.subscribe_to "Room/#{@name}/Commands" do |data|
+				@commandBlocks.each do |c| c.call(data); end
+			end
 		end
 
 		def lights=(newVal)
@@ -14,9 +22,19 @@ module Convenience
 				@mqtt.publish_to "Room/#{@name}/Lights/Set/Switch", "off", retain: true;
 			end
 		end
+		def lightColor
+			return @lights.value
+		end
+		def lightSwitch
+			return @lightSwitch.value == "on"
+		end
 
 		def command(cmd)
 			@mqtt.publish_to "Room/#{@name}/Commands", cmd
+		end
+
+		def on_command(&block)
+			@commandBlocks << block;
 		end
 	end
 end
