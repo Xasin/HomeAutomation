7da861b2a7317438e6eea215a145e8f5fa713f81
Xasin <logger89@web.de>
2018-05-07 16:14:55 +0200

    Fixed whitespace

--- Projects/MechaClock/ClockTest/MotorControl.cpp
+++ Projects/MechaClock/ClockTest/MotorControl.cpp
@@ -4,51 +4,54 @@
 
 namespace Motor {
 	volatile int32_t motorPosition = 0;
-	volatile int32_t lastMotor     = 0;
-	volatile int32_t motorTarget   = 0;
+	volatile int32_t lastMotor		 = 0;
+	volatile int32_t motorTarget	 = 0;
+	volatile int16_t motorSpeed	 = 0;
 
 	void setMotorPower(float pwr) {
-	  if(pwr < 0) {
-	    PORTB |= 1<< MOTOR_DIR;
-	    pwr = -1 * pwr;
-	  }
-	  else
-	    PORTB &= ~(1<< MOTOR_DIR);
+		if(pwr < 0) {
+			PORTB |= 1<< MOTOR_DIR;
+			pwr = -1 * pwr;
+		}
+		else
+			PORTB &= ~(1<< MOTOR_DIR);
 
-	  if(pwr < 0.02)
-	    pwr = 0;
-	  else if(pwr < 0.13)
-	    pwr = 0.13;
-	  else if(pwr > 1)
-	    pwr = 1;
+		if(pwr < 0.02)
+			pwr = 0;
+		else if(pwr < 0.13)
+			pwr = 0.13;
+		else if(pwr > 1)
+			pwr = 1;
 
-	  OCR1A = pwr*8000;
+		OCR1A = pwr*8000;
 	}
 
-  void updateEncoder() {
-    if((PIND>>MOTOR_C1 ^ PIND>>MOTOR_C2) & 1)
-       motorPosition++;
-     else
-       motorPosition--;
-  }
+	void updateEncoder() {
+		if((PIND>>MOTOR_C1 ^ PIND>>MOTOR_C2) & 1)
+			motorPosition++;
+		else
+			motorPosition--;
+	}
 
-  void updateMotor() {
-    setMotorPower(P_FACT * (motorTarget - motorPosition)
-            - D_FACT * (motorPosition - lastMotor));
-      lastMotor = motorPosition;
-  }
+	void updateMotor() {
+		motorSpeed = motorPosition - lastMotor;
+		lastMotor = motorPosition;
+
+		setMotorPower(	P_FACT * (motorTarget - motorPosition)
+							- D_FACT * motorSpeed);
+	}
 
 	void rotateTo(int32_t steps) {
-	  motorTarget = steps;
+		motorTarget = steps;
 
-	  int32_t mDiff = 0;
-	  while(true) {
-	   mDiff = (motorTarget - motorPosition);
-		if(fabs(mDiff) < 10)
-	      break;
-	  }
+		int32_t mDiff = 0;
+		while(true) {
+			mDiff = (motorTarget - motorPosition);
+			if(fabs(mDiff) < 10)
+			break;
+		}
 
-	  _delay_ms(50);
+		_delay_ms(10);
 	}
 
 	void init() {
@@ -64,11 +67,15 @@ namespace Motor {
 
 		TIMSK1|= (1<< TOIE1);
 
-		while(true) {
-		  motorTarget -= 100;
-		  _delay_ms(100);
-		  if(fabs(motorTarget - motorPosition) > 100)
-			 break;
+		uint8_t conseq_stops = 0;
+		while(conseq_stops < 10) {
+			motorTarget = motorPosition + 1000;
+			if(fabs(motorSpeed) < 1)
+				conseq_stops++;
+			else
+				conseq_stops = 0;
+
+			_delay_ms(5);
 		}
 
 		motorPosition 	= 0;
