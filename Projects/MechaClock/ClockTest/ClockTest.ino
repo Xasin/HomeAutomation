07e8ee0fb40eb175fc758bc2b235aee279a1084a
Xasin <logger89@web.de>
2018-05-15 11:31:00 +0200

    Added simplistic TWI interface

--- Projects/MechaClock/ClockTest/ClockTest.ino
+++ Projects/MechaClock/ClockTest/ClockTest.ino
@@ -1,11 +1,14 @@
 
+#include <avr/io.h>
+
 #include "MotorControl.h"
 #include "DigitControl.h"
 
-#define SEGMENTS  10
-#define DIGIT_NUM 3
-uint8_t currentSignPositions[DIGIT_NUM] = {0};
-uint8_t signDigits[DIGIT_NUM] = {0};
+#define MOTORS_ON
+#define MOTORS_OFF
+
+int16_t currentDial = 0;
+volatile int16_t targetDial = -1;
 
 uint8_t timerAPresc = 1;
 ISR(TIMER1_OVF_vect) {
@@ -20,57 +23,51 @@ ISR(INT0_vect) {
 	Motor::updateEncoder();
 }
 
-void dialDigit_raw(uint8_t digitNum) {
-	Motor::rotateTo(digitNum * STEPS_PER_REV / SEGMENTS);
+uint8_t TWI_writePos 	= 0;
+uint8_t TWI_writeBuffer = 0;
+ISR(TWI_vect) {
+	uint8_t TWSR_copy = TWSR & ~0b11;
+	switch(TWSR) {
+		case 0x60: // Address received
+			TWI_writePos = 0;
+		break;
 
-	currentSignPositions[0] = digitNum;
-	for(uint8_t i=1; i<DIGIT_NUM; i++) {
-		if(currentSignPositions[i] > digitNum)
-			currentSignPositions[i] = digitNum;
-		else if((currentSignPositions[i] + i*(SEGMENTS-1)) < digitNum)
-			currentSignPositions[i] = digitNum - i*(SEGMENTS-1);
-	}
-}
-
-void dialDigit(uint8_t digitNum, uint8_t segmentNum) {
-	uint8_t futurePosition = 0;
-
-	if(segmentNum == DIGIT_NUM)
-		futurePosition = digitNum;
-	else {
-		futurePosition = (currentSignPositions[segmentNum+1]/SEGMENTS)*SEGMENTS + digitNum;
-		if(futurePosition < currentSignPositions[segmentNum+1])
-		futurePosition += SEGMENTS;
+		case 0x80: // Data byte received
+			if(TWI_writePos == 0) {
+				TWI_writePos++;
+				TWI_writeBuffer = TWDR;
+			}
+			else {
+				targetDial = (TWI_writeBuffer | TWDR <<8);
+			}
+		break;
 	}
 
-	if(futurePosition <= currentSignPositions[segmentNum])
-		dialDigit_raw(futurePosition);
-	else
-		dialDigit_raw(futurePosition + (SEGMENTS-1)*segmentNum);
-}
-
-void printDigits() {
-	for(uint8_t i=0; i<NUM_DIGITS; i++) {
-		Serial.print(Digits::currentDigits[i]);
-		Serial.print(" ");
-	}
-	Serial.print("\n");
+	TWCR |= (1<< TWINT);
 }
 
 void setup() {
-	sei();
-
-	Serial.begin(9600);
-
 	Motor::init();
+
+	TWAR = 0x31 <<1;
+	TWCR = (1<< TWEA | 1<< TWIE | 1<< TWEN);
+
+	sei();
 }
 
-uint16_t dialNumber = 0;
 void loop() {
-	delay(3000);
-
-	dialNumber = random(0, 999);
-	Serial.println(dialNumber);
-	Digits::update_digits(dialNumber);
-	printDigits();
+	if(currentDial != targetDial) {
+		if(targetDial == -1) {
+			Digits::update_digits(0);
+			//MOTORS_OFF;
+			currentDial = -1;
+		}
+		else if(currentDial == -1) {
+			//MOTORS_ON;
+			Digits::home();
+			currentDial = 0;
+		}
+		else
+			Digits::update_digits(currentDial = targetDial);
+	}
 }
