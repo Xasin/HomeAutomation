1f464e7cbf4ae8514ecf5e19b775677ed704533e
Xasin <logger89@web.de>
2018-05-21 18:57:10 +0200

    Began separating TTS, Light and Hardware

---
+++ Projects/HomeSweetHome/LocalLib/LightController.rb
@@ -0,0 +1,69 @@
+
+require_relative '../Libs/ColorUtils.rb'
+require_relative 'TWILight.rb'
+
+require 'json'
+
+module Hardware
+	class Lights
+		def initialize(mqtt, light, room: "default", infoFadeTime: 0.5)
+			@mqtt  = mqtt;
+			@roomName = room;
+
+			@light = light;
+
+			@currentColor = Color.RGB(0, 0, 0);
+			@fadeTime = 10;
+			@infoFadeTime = infoFadeTime;
+
+			@fadeUntil = Time.new(0);
+
+			@currentOverride = nil;
+			@notificationBrightness = 100;
+
+			@mqtt.track "Room/#{@roomName}/Peripherals/Light" do |data|
+				begin
+					data = JSON.parse(data);
+
+					@fadeSpeed = data["fadeSpeed"].to_i or @fadeSpeed;
+
+					if(@data["skip"]) then
+						@fadeUntil = [@fadeUntil, Time.now() + @fadeTime].min;
+					else
+						@fadeUntil = Time.now() + @fadeTime;
+					end
+
+					@currentColor = Color.from_s(data["color"]);
+
+					update_color() unless @currentOverride;
+ 				end
+			end
+
+			@roomBrightness = @mqtt.track "Room/#{@roomName}/Sensors/Brightness"
+
+			@mqtt.track "Room/#{@roomName}/Info/Current" do |data|
+				begin
+					data = JSON.parse(data);
+					cO = Color.from_s(data["color"]);
+
+					cO.set_brightness
+						[5,
+						 @roomBrightness.value.to_i * 2.5,
+						 @currentColor.get_brightness].max
+
+					@currentOverride = cO;
+				rescue
+					@currentOverride = nil;
+				end
+
+				@fadeUntil = Time.now() + @infoFadeTime;
+				update_color();
+			end
+		end
+
+		def update_color()
+			@light.sendRGB([@currentOverride, @currentColor].compact[0],
+				@fadeUntil - Time.now());
+		end
+	end
+end
---
+++ Projects/HomeSweetHome/LocalLib/TWILight.rb
@@ -0,0 +1,34 @@
+require 'i2c'
+require_relative '../Libs/ColorUtils.rb'
+
+module Hardware
+	class RGB
+		def initialize(twiClass)
+			@twi ||= I2C.create("/dev/i2c-1");
+			@conAttempts = 0;
+
+			@lastColor = "";
+		end
+
+		def sendRGB(c, fadeDuration = 0)
+			return if @lastColor = c.to_s;
+			@lastColor = c.to_s;
+
+			fadeDuration *= 104.167;
+			begin
+			@twi.write(0x30, c.rgb.pack("C3"), [fadeDuration].pack("v"));
+			rescue Errno::EIO, Errno::ETIMEDOUT
+			@conAttempts += 1;
+			if @conAttempts == 5 then
+				puts "A TWI packet has been lost!"
+				@conAttempts = 0
+			else
+				sleep 0.5
+				retry
+			end
+			else
+			@conAttempts = 0
+			end
+		end
+	end
+end
