1825ba3e8ef51db910ccec77b810bdf4d88484b6
Xasin <logger89@web.de>
2018-05-28 17:42:04 +0200

    Tabbing

--- Projects/HomeSweetHome/LocalLib/TWILight.rb
+++ Projects/HomeSweetHome/LocalLib/TWILight.rb
@@ -16,16 +16,16 @@ module Hardware
 
 			fadeDuration *= 104.167;
 			begin
-			@twi.write(0x30, c.rgb.pack("C3"), [fadeDuration].pack("v"));
+				@twi.write(0x30, c.rgb.pack("C3"), [fadeDuration].pack("v"));
 			rescue Errno::EIO, Errno::ETIMEDOUT
-			@conAttempts += 1;
-			if @conAttempts == 5 then
-				puts "A TWI packet has been lost!"
-				@conAttempts = 0
-			else
-				sleep 0.5
-				retry
-			end
+				@conAttempts += 1;
+				if @conAttempts == 5 then
+					puts "A TWI packet has been lost!"
+					@conAttempts = 0
+				else
+					sleep 0.5
+					retry
+				end
 			else
 			@conAttempts = 0
 			end
