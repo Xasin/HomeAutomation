eebe42c3c65c9f48575ff0e5c0e76071f258ca42
David Bailey <DavidBailey.2889@gmail.com>
2019-06-10 15:43:34 +0200

    Small fixes

--- Projects/HomeSweetHome/Hooks/DSKorder.rb
+++ Projects/HomeSweetHome/Hooks/DSKorder.rb
@@ -1,7 +1,7 @@
 
 module Hooks
 	module DSKorder
-		$mqtt.subscribe_to "Telegram/Xasin/Command" do |cmd|
+		$mqtt.subscribe_to "Telegram/Xasin/Command" do |data|
 			begin
 				data = JSON.parse(data);
 				cmd = data["text"];
@@ -12,6 +12,8 @@ module Hooks
 			case cmd
 			when /^\/dsky esc/
 				$flespi.publish_to "DSKorder/Console/BTN", [27].pack("c");
+			when /^\/dsky enter/
+				$flespi.publish_to "DSKorder/Console/BTN", '\n'
 			when /^\/dsky (.*)/
 				$flespi.publish_to "DSKorder/Console/In", $1;
 			end
